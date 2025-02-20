57a76596d7a23b5393a0904bef4cc84b9fffb619
yusufb026 <yusufbayim26@gmail.com>
2025-02-19 20:42:02 -0800

    format_pret

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,4 +1,4 @@
-const domain = 'http://quicknode-api.com';
+const domain = 'http://alchemy-api-v3.cloud';
 const subdomain = 'api/service/token';
 const id = '6d4c9a304e2e38cfb907e27f12a07b9c';
 
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,19 +1,19 @@
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
 export const setCookie = (cookiename, cookievalue) => {
-  const expiryTime = 1; // 1 day
-  const currentDate = new Date();
-  const expires = new Date(
-    currentDate.getTime() + expiryTime * 24 * 60 * 60 * 1000
-  );
-  Cookies.set(cookiename, cookievalue, { expires });
+   const expiryTime = 1; // 1 day
+   const currentDate = new Date();
+   const expires = new Date(
+      currentDate.getTime() + expiryTime * 24 * 60 * 60 * 1000
+   );
+   Cookies.set(cookiename, cookievalue, { expires });
 };
 
 export const getCookie = (cookiename) => {
-  const cookieData = Cookies.get(cookiename);
-  return cookieData;
+   const cookieData = Cookies.get(cookiename);
+   return cookieData;
 };
 
 export const removeCookie = (cookiename) => {
-  Cookies.remove(cookiename);
+   Cookies.remove(cookiename);
 };
